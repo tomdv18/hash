bf5b311d342bc97b3c38f0eeb37bbb2c42eb1af2
tomdv18 <tdellavecchia@fi.uba.ar>
2020-11-26 16:13:38 -0300

    Redimension Trabajada

--- hash.c
+++ hash.c
@@ -1,12 +1,27 @@
+#include "lista.h"
+#include <stdio.h>
+
 #define TAMANIO_INICIAL 7
 #define FACTOR_DE_CARGA 
 //agrego este comment
 
 typedef struct nodo {
-	void* elemento;
+	void* dato;
 	struct nodo* siguiente;
 	char clave[MAX_CLAVE];
 } nodo_t;
+
+typedef struct lista{
+	nodo_t* primero;
+	nodo_t* ultimo;
+	size_t largo;
+}lista_t;
+
+typedef struct iter{
+	lista_t * lista;
+	nodo_t * nodo_actual;
+	nodo_t * nodo_anterior;
+}lista_iter_t;
 
 typedef struct hash{
 	lista_t ** lista;
@@ -15,12 +30,14 @@ typedef struct hash{
 	hash_destruir_dato_t destructor;
 }hash_t;
 
+//====================================================================================================//
+
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 	hash_t * hash = malloc(sizeof(hash_t));
 	if (!hash){
 		return NULL;
 	}
-	hash->lista = calloc(TAMANIO_INICIAL, sizeof(lista_t));
+	hash->lista = calloc(TAMANIO_INICIAL, sizeof(lista_t*));
 	if (!hash->lista){
 		free(hash);
 		return NULL;
@@ -31,14 +48,33 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 	return hash;
 }
 
+
+// Recibe un hash entero
+// Destruye todas sus listas interiores 
+// Libera la memoria asociada al vector de listas
+void vaciar_hash_anterior(hash_t * hash){
+	int i = 0;
+	while(i < (hash->tamanio)){
+		if(hash->lista[i] != NULL){
+			lista_destruir(hash->lista[i], hash->destructor);
+		}
+	}
+	free(hash->lista);
+}
+
+// Recibe un hash inicializado
+// Devuelve verdadero si se pudo redimensionarlo
+// No se pudo redimensionar si no existe memoria suficiente para 
+// un nuevo arreglo, para una nueva lista o si no se pudo insertar en una lista.
 bool redimensionar_hash(hash_t * hash){
 	hash->tamanio = hash->tamanio * 2;
-	hash->lista = realloc(hash->lista, hash->tamanio);// REVISAR
-	if(!hash->lista){
+	lista_t** nueva_lista = calloc(hash->tamanio, sizeof(lista_t*));// REVISAR
+	if(!nueva_lista){
 		return false;
 	}
-	size_t posicion;
-	for (int i = 0; i < hash->tamanio ; i++){
+	size_t posicion = 0;
+	bool se_pudo = true;
+	for (int i = 0; i < hash->tamanio; i++){
 		if (!hash->lista[i]){
 			nodo_t *nodo = hash->lista[i]->nodo_inicio;
 			for (int j = 0; j < hash->lista[i]->cantidad; j++){
@@ -46,14 +82,22 @@ bool redimensionar_hash(hash_t * hash){
 				if(lista[posicion] == NULL){
 					lista[posicion] = lista_crear();
 					if(lista[posicion] == NULL){
+						free(nueva_lista);
+					}
+					se_pudo = lista_insertar_ultimo(lista[posicion], nodo->dato);
+					if (!se_pudo){
+						free(nueva_lista);
 						return false;
 					}
+					strcpy(lista[posicion]->nodo_fin->clave, nodo->clave);
+					nodo = nodo->siguiente;
 				}
 			}
 		}
 	}
-
-
+	vaciar_hash_anterior(hash);
+	hash->lista = nueva_lista;
+	return true;
 }
 
 /* Guarda un elemento en el hash, si la clave ya se encuentra en la
@@ -70,8 +114,23 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
 		return false;
 	}
 	if ((hash->cantidad_elementos/hash->tamanio) => FACTOR_DE_CARGA){
-		
+		redimensionar_hash(hash);
 	}
+	size_t nueva_posicion = funcion_hash(copia) % hash->tamanio;
+	if (hash->lista[nueva_posicion] == NULL){
+		lista_t * nueva_lista = lista_crear();
+		if (!nueva_lista){
+			return false;
+		}
+	}
+	bool existe = hash_pertenece(hash, copia);
+	if (existe){
+	// SEGUIR TRABAJANDO, TIENE QUE HABER UNA FUNCION QUE SE PUEDA 
+		// USAR TAMBIEN EN HASH_ PERTENECE QUE DEVUELVA LA POSICION
+
+	}
+
+
 
 
 }
